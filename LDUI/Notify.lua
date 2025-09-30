0970db87bea65758319f52373a21b92985cdc04e
c3iv3r <kaelzsec@gmail.com>
2025-09-30 17:36:55 +0700

    Update Notify.lua

--- LDUI/Notify.lua
+++ LDUI/Notify.lua
@@ -2,22 +2,22 @@
 local Themes = {}
 
 Themes.Dark = {
-    WindowBackground = Color3.fromRGB(20, 20, 20),
+    WindowBackground = Color3.fromRGB(0, 0, 0),
     WindowBorder = Color3.fromRGB(40, 40, 40),
     WindowStroke = Color3.fromRGB(60, 60, 60),
     
-    TopFrameBackground = Color3.fromRGB(20, 20, 20),
+    TopFrameBackground = Color3.fromRGB(0, 0, 0),
     TopFrameBorder = Color3.fromRGB(40, 40, 40),
     
-    TabButtonsBackground = Color3.fromRGB(20, 20, 20),
+    TabButtonsBackground = Color3.fromRGB(0, 0, 0),
     TabButtonsBorder = Color3.fromRGB(40, 40, 40),
     
-    TabContentBackground = Color3.fromRGB(28, 28, 28),
+    TabContentBackground = Color3.fromRGB(0, 0, 0),
     
-    SectionBackground = Color3.fromRGB(28, 28, 28),
+    SectionBackground = Color3.fromRGB(0, 0, 0),
     SectionStroke = Color3.fromRGB(28, 28, 28),
     
-    ElementBackground = Color3.fromRGB(35, 35, 35),
+    ElementBackground = Color3.fromRGB(0, 0, 0),
     ElementStroke = Color3.fromRGB(35, 35, 35),
     ElementStrokeHover = Color3.fromRGB(10, 135, 213),
     ElementStrokeLocked = Color3.fromRGB(40, 40, 40),
