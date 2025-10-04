97735f1b68101d3743f9338cc1c4fc1215dbdfbe
c3iv3r <kaelzsec@gmail.com>
2025-10-05 00:05:03 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1154,7 +1154,7 @@ function Library:Window(p)
 	ChSize_1.Image = "rbxassetid://15082210525"
 	ChSize_1.ImageTransparency = 0.5
 
-	DropdownValue_1.Name = "DropdownValue"
+	--[[DropdownValue_1.Name = "DropdownValue"
 	DropdownValue_1.Parent = Ct_1
 	DropdownValue_1.AnchorPoint = Vector2.new(1, 0.5)
 	DropdownValue_1.BackgroundColor3 = Color3.fromRGB(24,24,31)
@@ -1162,7 +1162,7 @@ function Library:Window(p)
 	DropdownValue_1.BorderSizePixel = 0
 	DropdownValue_1.Position = UDim2.new(1, 0,0.5, 0)
 	DropdownValue_1.Size = UDim2.new(0, 20, 0, 20)
-	DropdownValue_1.Transparency = 1
+	DropdownValue_1.Transparency = 1]]
 
 	Td_1.Name = "Td"
 	Td_1.Parent = Topbar_1
@@ -4551,7 +4551,7 @@ function Library:Window(p)
 			})
 		end
 	
-		local ThemeDrop = addDropdownSelect(DropdownValue_1, DropdownValue_1, false, CallTheme, Theme, themes.index)
+		--local ThemeDrop = addDropdownSelect(DropdownValue_1, DropdownValue_1, false, CallTheme, Theme, themes.index)
 
 		Close_1.MouseButton1Click:Connect(function()
 			Tabs:Dialog({
